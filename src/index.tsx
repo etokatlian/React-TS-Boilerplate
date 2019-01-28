8d26a3b0080ea239044693fa0690f038e4869c12
eric-techfabric <eric@techfabric.co>
2019-01-28 16:05:33 -0700

    Adds folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import {Hello} from "./components/Hello";
+import { Hello } from "./features/Hello";
 
-ReactDOM.render(
-    <Hello/>,
-    document.getElementById("app")
-);
+ReactDOM.render(<Hello />, document.getElementById("app"));
