ec5286da9500b3dc08c97768d12bb573afc29686
eric-techfabric <eric@techfabric.co>
2019-01-28 14:47:00 -0700

    Adds jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,16 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
+  collectCoverage: true,
+
+  // collectCoverageFrom indicates which paths to include coverage. Bang indicates ignoring this path
+  collectCoverageFrom: [
+    "**/*.{ts,tsx,js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**",
+    "!**/dist/**",
+    "!**/coverage/**"
+  ],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -4,15 +4,8 @@ export interface HelloProps {}
 
 export interface HelloState {}
 
-//'HelloProps' describes the shape of props.
-//'HelloState' describes the shape of props.
 export class Hello extends React.Component<HelloProps, HelloState> {
-  render(): Array<JSX.Element> {
-    return [
-      <h1>React/TS/Webpack Boilerplate</h1>,
-      <h3>
-        We have successfully setup our React, Webpack, and Typescript project!
-      </h3>
-    ];
+  render() {
+    return <h1>React/TS Boilerplate</h1>;
   }
 }
