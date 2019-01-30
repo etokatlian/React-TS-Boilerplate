88c07cd08b274ffce57896000098da706dfe9e9b
eric-techfabric <eric@techfabric.co>
2019-01-30 09:38:48 -0700

    Add normalization for sass

--- webpack.common.js
+++ webpack.common.js
@@ -28,19 +28,35 @@ module.exports = {
         test: /\.js$/,
         loader: 'source-map-loader',
       },
+      // Resolve png/jpg/gif into url and emit file into the output directory
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              mimetype: "image/png",
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      },
       // Resolve svg into url and emit file into the output directory
       {
-        test: /\.(svg|jpg|gif)$/,
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 8192,
-            },
-          },
-        ],
-      },
-    ],
+              mimetype: "image/svg+xml",
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
   },
 
   plugins: [
