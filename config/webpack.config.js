e99323ec21f2173ec2ca3c5bae5f034820a59adb
eric-techfabric <eric@techfabric.co>
2019-01-30 12:41:09 -0700

    Move build configs to separate directory

---
+++ config/webpack.config.js
@@ -0,0 +1,30 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const path = require("path");
+
+module.exports = merge(common, {
+  mode: "development",
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+
+  // Dev server options
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+    inline: true
+  }
+});
